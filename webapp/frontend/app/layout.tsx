359a2ae6b7cd2b123b966ef6150e61df3fbd1a59
Max8Curtis <mcurtis01@outlook.com>
2025-10-20 14:38:33 +0100

    migrate to NextJS

---
+++ webapp/frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import '../index.css'
+
+export const metadata: Metadata = {
+    title: 'Japanese Translation',
+    description: 'Application for practicing Japanese translation.',
+}
+
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <div id="root">{children}</div>
+            </body>
+        </html>
+    )
+}
