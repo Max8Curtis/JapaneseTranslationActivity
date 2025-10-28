6a2022a635a839cac11719428216aa5e8df11fd5
Max8Curtis <mcurtis01@outlook.com>
2025-10-28 18:58:45 +0000

    Get English Example text and new DB tables

--- webapp/backend/tables.py
+++ webapp/backend/tables.py
@@ -8,6 +8,34 @@ def init_db(conn):
             name TEXT UNIQUE
         );
     """)
+
+    curr.execute("""
+        CREATE TABLE IF NOT EXISTS example (
+            id SERIAL PRIMARY KEY,
+            text_jp TEXT,
+            text_en TEXT,
+            highlight_indices TEXT,
+            grammar INT,
+            FOREIGN KEY grammar REFERENCES grammar(id)
+        );
+                """)
+
+    curr.execute("""
+        CREATE TABLE IF NOT EXISTS grammar (
+            id SERIAL PRIMARY KEY,
+            level NOT NULL,
+            description TEXT,
+            FOREIGN KEY (level) REFERENCES level(id);
+        );
+                 """)
+    
+    curr.execute("""
+        CREATE TABLE IF NOT EXISTS level (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(2)
+        );
+                 """)
+
     conn.commit()
     curr.close()
 
